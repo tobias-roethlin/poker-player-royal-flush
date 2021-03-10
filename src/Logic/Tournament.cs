570b73aa44b5ba12c21a6f840894eec19ecde979
renaudrenaud85 <renaud.raphael@gmail.com>
2021-03-10 14:38:10 +0100

    Minor

--- src/Logic/Tournament.cs
+++ src/Logic/Tournament.cs
@@ -10,5 +10,14 @@ namespace Nancy.Simple.Logic
         public int Round { get; set; }
         public int CurrentBuyIn { get; set; }
         public int Pot { get; set; }
+        public IEnumerable<Card> GetCards()
+        {
+            yield return OurPlayer.Card1;
+            yield return OurPlayer.Card2;
+            foreach (var card in CommunityCards)
+            {
+                yield return card;
+            }
+        }
     }
 }
