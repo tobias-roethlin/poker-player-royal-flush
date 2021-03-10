03f44c24a9e28bf47c27dc08bad74584801cb06a
renaudrenaud85 <renaud.raphael@gmail.com>
2021-03-10 11:34:52 +0100

    Minor

--- src/Logic/LogicService.cs
+++ src/Logic/LogicService.cs
@@ -24,10 +24,12 @@ namespace Nancy.Simple.Logic
             var probabilities = GetProbabilities();
             var card1String = tournament.OurPlayer.Card1.GetString();
             var card2String = tournament.OurPlayer.Card1.GetString();
+            var firstCombination = $"{card1String}{card2String}";
+            var secondCombination = $"{card2String}{card1String}";
             double probability = 0;
-            if (!probabilities.TryGetValue(card1String + card2String, out probability))
+            if (!probabilities.TryGetValue(firstCombination, out probability))
             {
-                probabilities.TryGetValue(card2String + card1String, out probability);
+                probabilities.TryGetValue(secondCombination, out probability);
             }
 
             return (int)(tournament.OurPlayer.Stack * probability);
