93b3e07a9487b133bca9784f6ac5a2f8b60f0db7
Tobias Roethlin <Tobias.Roethlin@digitecgalaxus.ch>
2021-03-10 13:35:10 +0100

    Fix problem

--- src/Logic/LogicService.cs
+++ src/Logic/LogicService.cs
@@ -272,7 +272,7 @@ namespace Nancy.Simple.Logic
 
             player.Bet = playerJson.bet;
             
-            if (playerJson != null && playerJson.hole_cards.Count >= 2)
+            if (playerJson.hole_cards != null && playerJson.hole_cards.Count >= 2)
             {
                 player.Card1 = CardMapper(playerJson.hole_cards[0]);
                 player.Card2 = CardMapper(playerJson.hole_cards[1]);
