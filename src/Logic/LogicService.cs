554221befc919d59985193fe014a1b6f45c85c62
renaudrenaud85 <renaud.raphael@gmail.com>
2021-03-10 15:12:11 +0100

    Minor

--- src/Logic/LogicService.cs
+++ src/Logic/LogicService.cs
@@ -40,10 +40,14 @@ namespace Nancy.Simple.Logic
             }
 
             var considerAllIn = false;
-            var betValue = tournament.OurPlayer.Stack * (1.0 / 100 * probability / 2);
+            var betValue = 0.0;
             if (tournament.Round > 0)
             {
                 betValue = tournament.OurPlayer.Stack * (1.0 / 100 * probability / 10);
+            }
+            else
+            {
+                betValue = tournament.OurPlayer.Stack * (1.0 / 100 * probability / 2);
             }
 
             if (tournament.Round == 0 && probability > 0.5)
