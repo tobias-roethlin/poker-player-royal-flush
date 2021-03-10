0cdf34d70a9479c564078340e2a6e8da1021687c
Tobias Roethlin <Tobias.Roethlin@digitecgalaxus.ch>
2021-03-10 10:31:54 +0100

    Add data structur

---
+++ src/Card.cs
@@ -0,0 +1,20 @@
+﻿namespace Nancy.Simple
+{
+    public class Card
+    {
+        public string rank { get; set; }                // Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+        public string suit { get; set; }  
+    }
+    
+    /*"hole_cards": [                         // The cards of the player. This is only visible for your own player
+    //     except after showdown, when cards revealed are also included.
+    {
+        "rank": "6",                    // Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+        "suit": "hearts"                // Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+    },
+    {
+    "rank": "K",
+    "suit": "spades"
+}
+]*/
+}
---
+++ src/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Nancy.Simple
+{
+    public class Player
+    {
+        public string name { get; set; }
+        
+        public string stack { get; set; }
+        
+        public string status { get; set; }
+        
+        public int bet { get; set; }
+
+        public List<Card> hole_cards { get; set; } = new List<Card>();
+        
+        public string version { get; set; }
+        
+        public int id { get; set; }
+    }
+    
+    /*"players":[
+    {
+        "name":"Player 1",
+        "stack":1000,
+        "status":"active",
+        "bet":0,
+        "hole_cards":[],
+        "version":"Version name 1",
+        "id":0
+    },*/
+}
